90cc6fd24a3ed40bb5cd9eb7f1f4cd81d1ce4fe7
Marcus Elg <melg@kth.se>
2022-01-23 09:39:45 +0100

    I think it accually works now

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,30 +3,46 @@ use std::io::prelude::*;
 use std::io::stdin;
 use std::net::TcpStream;
 use std::str;
+use std::thread;
+use std::time;
 
 fn main() {
     // 127.0.0.1 is address, 7878 is port
     let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
 
-    loop {
-        println!("Enter a message:");
+    // Send messages
+    let mut stream_copy = stream.try_clone().unwrap();
+    thread::spawn(move || {
+        loop {
+            println!("Enter a message:");
 
-        let mut input = String::new();
-        stdin().read_line(&mut input).unwrap();
-        match stream.write(input.as_bytes()) {
-            Ok(_) => {
-                let mut buffer = [0; 50];
-                match stream.read(&mut buffer) {
-                    Ok(size) => {
-                        println!("Returned: {}", str::from_utf8(&buffer[0..size]).unwrap());
-                    }
-                    Err(_) => {}
+            let mut input = String::new();
+            stdin().read_line(&mut input).unwrap();
+            match stream_copy.write(input.as_bytes()) {
+                Ok(_) => {
+                    // Successfully sent message
+                }
+                Err(_) => {
+                    // Exit if server has closed
+                    break;
                 }
             }
-            Err(_) => {
-                // Exit if server has closed
-                break;
-            }
+
+            thread::sleep(time::Duration::from_secs(1));
         }
+    });
+
+    // Print messages
+    loop {
+        let mut buffer = [0; 50];
+        match stream.read(&mut buffer) {
+            Ok(0) => {}
+            Ok(size) => {
+                println!("Returned: {}", str::from_utf8(&buffer[0..size]).unwrap());
+            }
+            Err(_) => {}
+        }
+
+        thread::sleep(time::Duration::from_secs(1));
     }
 }
--- sockets/src/main.rs
+++ sockets/src/main.rs
@@ -1,23 +1,73 @@
 use std::io::prelude::*;
+use std::io::ErrorKind;
 use std::net::{Shutdown, TcpListener, TcpStream};
 use std::str;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
+use std::time;
 
 fn main() {
   // 127.0.0.1 is address, 7878 is port
   let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+  let (send, recieve) = mpsc::channel();
+  let (send_index, recieve_index) = mpsc::channel();
+  let mut streams = Vec::new();
+  let mut stream_index = 100;
 
-  for stream in listener.incoming() {
-    thread::spawn(|| {
-      let stream = stream.unwrap();
-      handle_connection(stream);
-    });
+  listener.set_nonblocking(true).unwrap(); // Prevent listener.accept blocking the thread
+
+  // Main loop
+  loop {
+    // Listen for clients
+    match listener.accept() {
+      Ok((mut stream, _)) => {
+        streams.push(stream.try_clone().unwrap());
+        if stream_index == 100 {
+          stream_index = 0;
+        } else {
+          stream_index += 1;
+        }
+
+        let local_sender = send.clone();
+        let local_index_sender = send_index.clone();
+
+        thread::spawn(move || {
+          handle_connection(stream, local_sender, local_index_sender, stream_index);
+        });
+      }
+      Err(_) => {}
+    }
+
+    // Go through all threads and send them the message that the server recieved
+    loop {
+      match recieve.try_recv() {
+        Ok(message) => {
+          if message.len() > 0 {
+            println!("{}", message);
+            let source_stream_index = recieve_index.recv().unwrap();
+            println!("{:?}", source_stream_index);
+            for i in 0..(&streams).len() {
+              if i != source_stream_index {
+                streams[i].write(message.as_bytes()).unwrap();
+              }
+            }
+          }
+        }
+        Err(_) => break,
+      }
+
+      thread::sleep(time::Duration::from_secs(1));
+    }
   }
-
-  drop(listener);
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(
+  mut stream: TcpStream,
+  send: Sender<String>,
+  send_index: Sender<usize>,
+  stream_index: usize,
+) {
   let mut buffer = [0 as u8; 50];
 
   // Always listen for data
@@ -29,19 +79,27 @@ fn handle_connection(mut stream: TcpStream) {
         break;
       }
       Ok(size) => {
+        let message = str::from_utf8(&buffer[0..size]).unwrap();
         println!(
-          "Message has been recieved on server: {}",
-          str::from_utf8(&buffer[0..size]).unwrap()
+          "Message has been recieved on server: {} by {}",
+          message, stream_index
         );
-        stream.write(&buffer[0..size]).unwrap();
-        stream.flush().unwrap();
+        send.send(message.to_owned()).unwrap();
+        send_index.send(stream_index).unwrap();
       }
       Err(e) => {
+        // Don't close connection
+        if e.kind() == ErrorKind::WouldBlock {
+          continue;
+        }
+
         println!("{:?}", e);
         println!("Connection has been closed");
         stream.shutdown(Shutdown::Both).unwrap();
         break;
       }
     }
+
+    thread::sleep(time::Duration::from_secs(1));
   }
 }
